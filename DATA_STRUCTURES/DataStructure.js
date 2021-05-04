7dc5b89dfd46555bed72787afad4c0f4e707e54e
Cannigia Laluw <cannigialaluw@gmail.com>
2021-05-03 23:20:34 -0400

    starting on trees

--- DATA_STRUCTURES/DataStructure.js
+++ DATA_STRUCTURES/DataStructure.js
@@ -1,3 +1,5 @@
+// ------------------------- ARRAYS --------------------------- //
+
 class myArray {
     constructor(){
         this.length = 0;
@@ -45,7 +47,7 @@ class myArray {
 // console.log(newArray)
 
 
-//Hash Table
+// ------------------------- HASH TABLE --------------------------- //
 class HashTable {
     constructor(size){
       this.data = new Array(size);
@@ -89,7 +91,8 @@ class HashTable {
 //   myHashTable.set('apples', 9)
 //   myHashTable.get('apples')
 
-// LinkedList
+
+// ------------------------- LINKED LISTS --------------------------- //
 // Create the below linked list:
 // myLinkedList = {
 //   head: {
@@ -102,7 +105,9 @@ class HashTable {
 //       }
 //     }
 //   }
-// };
+// }
+
+// Singly Linked List
 class LinkedList {
   constructor(value) {
     this.head = {
@@ -112,33 +117,382 @@ class LinkedList {
     this.tail = this.head;
     this.length = 1;
   }
-
   append(value) {
     const newNode = {
       value: value,
-      next: null,
-    };
+      next: null
+    }
+    console.log(newNode)
     this.tail.next = newNode;
     this.tail = newNode;
     this.length++;
-    return this
+    return this;
   }
-
   prepend(value) {
     const newNode = {
       value: value,
       next: null
-    };
-    newNode.next = this.head
+    }
+    newNode.next = this.head;
     this.head = newNode;
     this.length++;
-    return this
+    return this;
+  }
+  printList() {
+    const array = [];
+    let currentNode = this.head;
+    while(currentNode !== null){
+        array.push(currentNode.value)
+        currentNode = currentNode.next
+    }
+    return array;
+  }
+  insert(index, value){
+    //Check for proper parameters;
+    if(index >= this.length) {
+      console.log('yes')
+      return this.append(value);
+    }
+  
+    const newNode = {
+      value: value,
+      next: null
+    }
+    const leader = this.traverseToIndex(index-1);
+    const holdingPointer = leader.next;
+    leader.next = newNode;
+    newNode.next = holdingPointer;
+    this.length++;
+    return this.printList();
+  }
+  traverseToIndex(index) {
+    //Check parameters
+    let counter = 0;
+    let currentNode = this.head;
+    while(counter !== index){
+      currentNode = currentNode.next;
+      counter++;
+    }
+    return currentNode;
+  }
+  remove(index) {
+    // Check Parameters      
+    const leader = this.traverseToIndex(index-1);
+    const unwantedNode = leader.next;
+    leader.next = unwantedNode.next;
+    this.length--;
+    return this.printList();
+  }
+  reverse(){
+    if(!this.head.next){
+      return this.head;
+    }
+    let first = this.head;
+    this.tail = this.head
+    let second = first.next
+
+    while(second){
+      const temp = second.next
+      second.next = first;
+      first = second
+      second = temp
+    }
+
+    this.head.next = null;
+    this.head = first;
+    return this.printList();
   }
 }
 
 // let myLinkedList = new LinkedList(10);
-// myLinkedList.append(5);
-// myLinkedList.append(16);
+// myLinkedList.append(5)
+// myLinkedList.append(16)
+// myLinkedList.prepend(1)
+// myLinkedList.printList()
+// myLinkedList.insert(2, 99)
+// myLinkedList.insert(20, 88)
+// myLinkedList.printList()
+// myLinkedList.remove(2)
+// myLinkedList.reverse()
+
+// Doubly linked list
+class DoublyLinkedList {
+  constructor(value) {
+    this.head = {
+      value: value,
+      next: null,
+      prev: null
+    };
+    this.tail = this.head;
+    this.length = 1;
+  }
+  append(value) {
+    const newNode = {
+      value: value,
+      next: null,
+      prev: null
+    }
+    console.log(newNode)
+    newNode.prev = this.tail
+    this.tail.next = newNode;
+    this.tail = newNode;
+    this.length++;
+    return this;
+  }
+  prepend(value) {
+    const newNode = {
+      value: value,
+      next: null,
+      prev: null
+    }
+    newNode.next = this.head;
+    this.head.prev = newNode
+    this.head = newNode;
+    this.length++;
+    return this;
+  }
+  printList() {
+    const array = [];
+    let currentNode = this.head;
+    while(currentNode !== null){
+      array.push(currentNode.value)
+      currentNode = currentNode.next
+    }
+    return array;
+  }
+  insert(index, value){
+    //Check for proper parameters;
+    if(index >= this.length) {
+      return this.append(value);
+    }
+    
+    const newNode = {
+      value: value,
+      next: null,
+      prev: null
+    }
+    const leader = this.traverseToIndex(index-1);
+    const follower = leader.next;
+    leader.next = newNode;
+    newNode.prev = leader;
+    newNode.next = follower;
+    follower.prev = newNode;
+    this.length++;
+    console.log(this)
+    return this.printList();
+  }
+  traverseToIndex(index) {
+    //Check parameters
+    let counter = 0;
+    let currentNode = this.head;
+    while(counter !== index){
+      currentNode = currentNode.next;
+      counter++;
+    }
+    return currentNode;
+  }
+}
+
+let myLinkedList = new DoublyLinkedList(10);
+myLinkedList.append(5)
+myLinkedList.append(16)
+myLinkedList.prepend(1)
+myLinkedList.insert(2, 99)
+// myLinkedList.insert(20, 88)
+// myLinkedList.printList()
+// myLinkedList.remove(2)
+// myLinkedList.reverse()
+
+// ------------------------- STACKS --------------------------- //
+// Linked list implementation
+class Node {
+  constructor(value){
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Stack {
+  constructor(){
+    this.top = null;
+    this.bottom = null;
+    this.length = 0;
+  }
+  peek() {
+    return this.top;
+  }
+  push(value){
+    const newNode = new Node(value);
+    if (this.length === 0) {
+      this.top = newNode;
+      this.bottom = newNode;
+    } else {
+      const holdingPointer = this.top;
+      this.top = newNode;
+      this.top.next = holdingPointer;
+    }
+    this.length++;
+    return this;
+  }
+  pop(){
+    if (!this.top) {
+      return null;
+    }
+    if (this.top === this.bottom) {
+      this.bottom = null;
+    }
+    const holdingPointer = this.top;
+    this.top = this.top.next;
+    this.length--;
+    return this;
+  }
+  //isEmpty
+}
+
+const myStack = new Stack();
+myStack.peek();
+myStack.push('google');
+myStack.push('udemy');
+myStack.push('discord');
+myStack.peek();
+myStack.pop();
+myStack.pop();
+myStack.pop();
 
 
+//Discord
+//Udemy
+//google
 
+// Stacks Array implementation
+class Stack {
+  constructor(){
+    this.array = [];
+  }
+  peek() {
+    return this.array[this.array.length-1];
+  }
+  push(value){
+    this.array.push(value);
+    return this;
+  }
+  pop(){
+    this.array.pop();
+    return this;
+  }
+}
+
+const myStack = new Stack();
+myStack.peek();
+myStack.push('google');
+myStack.push('udemy');
+myStack.push('discord');
+myStack.peek();
+myStack.pop();
+myStack.pop();
+myStack.pop();
+
+
+//Discord
+//Udemy
+//google
+
+// ------------------------- QUEUE --------------------------- //
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Queue {
+  constructor(){
+    this.first = null;
+    this.last = null;
+    this.length = 0;
+  }
+  peek() {
+    return this.first;
+  }
+  enqueue(value){
+    const newNode = new Node(value);
+    if (this.length === 0) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      this.last.next = newNode;
+      this.last = newNode;
+    }
+    this.length++;
+    return this;
+  }
+  dequeue(){
+    if (!this.first) {
+      return null;
+    }
+    if (this.first === this.last) {
+      this.last = null;
+    }
+    const holdingPointer = this.first;
+    this.first = this.first.next;
+    this.length--;
+    return this;
+  }
+  //isEmpty;
+}
+
+const myQueue = new Queue();
+myQueue.peek();
+myQueue.enqueue('Joy');
+myQueue.enqueue('Matt');
+myQueue.enqueue('Pavel');
+myQueue.peek();
+myQueue.dequeue();
+myQueue.dequeue();
+myQueue.dequeue();
+myQueue.peek();
+
+
+// implementing queues using stacks
+class CrazyQueue {
+  constructor() {
+    this.first = [];
+    this.last = [];
+  }
+
+  enqueue(value) {
+    const length = this.first.length;
+    for (let i = 0; i < length; i++) {
+      this.last.push(this.first.pop());
+    }
+    this.last.push(value);
+    return this;
+  }
+
+  dequeue() {
+    const length = this.last.length;
+    for (let i = 0; i < length; i++) {
+      this.first.push(this.last.pop());
+    }
+    this.first.pop();
+    return this;
+  }
+  peek() {
+    if (this.last.length > 0) {
+      return this.last[0];
+    }
+    return this.first[this.first.length - 1];
+  }
+}
+
+const myQueue = new CrazyQueue();
+myQueue.peek();
+myQueue.enqueue('Joy');
+myQueue.enqueue('Matt');
+myQueue.enqueue('Pavel');
+myQueue.peek();
+myQueue.dequeue();
+myQueue.dequeue();
+myQueue.dequeue();
+myQueue.peek();
